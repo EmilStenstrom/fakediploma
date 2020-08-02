3741802db0dbaf8556122aa650581480ec687486
Emil Stenström <em@kth.se>
2020-08-02 13:59:56 +0200

    Use modern version of run.py.

--- run.py
+++ run.py
@@ -2,17 +2,42 @@
 import os
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--run', action="store_true")
-parser.add_argument('--deploy', action="store_true")
-parser.add_argument('--get_universities', action="store_true")
-args = parser.parse_args()
+def run():
+    """ Reuse the Procfile to start the dev server """
+    with open("Procfile", "r") as f:
+        command = f.read().strip()
 
-if not any(vars(args).values()):
-    parser.print_help()
-elif args.run:
-    os.system("gunicorn server:app --reload --config gunicorn_config.py")
-elif args.deploy:
-    os.system("git push heroku master")
-elif args.get_universities:
+    command = command.replace("web: ", "")
+    command += " --reload"
+    os.system(command)
+
+def deploy():
+    os.system("git push dokku master")
+
+def dependencies():
+    os.system("pip-compile --upgrade requirements.in")
+    os.system("pip-compile --upgrade requirements-dev.in")
+    os.system("pip-sync requirements-dev.txt")
+
+def get_universities():
     os.system("curl https://raw.githubusercontent.com/Hipo/university-domains-list/master/world_universities_and_domains.json --output universities.json")
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--deploy', action="store_true", required=False)
+    parser.add_argument('--deps', action="store_true", required=False)
+    parser.add_argument('--get_universities', action="store_true", required=False)
+    args = parser.parse_args()
+
+    if args.deploy:
+        deploy()
+    elif args.deps:
+        dependencies()
+    elif args.get_universities:
+        get_universities()
+    else:
+        run()
+
+
+if __name__ == '__main__':
+    main()
